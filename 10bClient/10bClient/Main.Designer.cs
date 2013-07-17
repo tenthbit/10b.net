ea082f55604ee8c4820b684703a8699c68785905
Liam Dunn <liam.dunn144@gmail.com>
2013-07-17 17:24:09 +1000

    Make txtMsg use Consolas too

--- 10bClient/10bClient/Main.Designer.cs
+++ 10bClient/10bClient/Main.Designer.cs
@@ -101,6 +101,7 @@
             this.txtMsg.AcceptsTab = true;
             this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsg.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtMsg.Location = new System.Drawing.Point(12, 567);
             this.txtMsg.Multiline = true;
             this.txtMsg.Name = "txtMsg";
